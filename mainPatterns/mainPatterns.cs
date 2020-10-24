d5b3b9eaaf7ff085fb1d6d8fb1d8e932e4aa8b81
Kusanagi-2029 <sonyandpc87@gmail.com>
2020-10-24 14:22:37 +0300

    added patterns - tutorials on C#

---
+++ mainPatterns/mainPatterns.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainPatterns
+{
+    class mainPatterns
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     1) Abstract Factory pattern: \n");
+            new Client().Main();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
